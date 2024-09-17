8a6567ff1dd33b598febec49b0037cd0d3558df2
sanjeev662 <sanjeevsinghkaushik662@gmail.com>
2024-09-17 13:35:55 +0530

    set secure to true for cookie

--- backend/services/scraperService.js
+++ backend/services/scraperService.js
@@ -13,7 +13,7 @@ app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // Set to true if using HTTPS
+  cookie: { secure: true } // Set to true if using HTTPS
 }));
 
 const scrapeAuctions = async (startPage, endPage, username, password, sortBy, sortDirection, sendData) => {
