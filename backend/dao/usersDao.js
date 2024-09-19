e760cefed0e4d32ef7f9d285270798c46230ce86
sanjeev662 <sanjeevsinghkaushik662@gmail.com>
2024-09-19 11:36:22 +0530

    feat: register

--- backend/dao/usersDao.js
+++ backend/dao/usersDao.js
@@ -1,7 +1,16 @@
-module.exports = [
-    { username: 'sanjeev', password: 'sanjeev@12666' },
-    { username: 'yash', password: 'yash@12666' },
-    { username: 'prakhar', password: 'prakhar@12666' },
-    { username: 'test', password: 'test@12666' },
-    { username: 'admin', password: 'admin@12666' }
-  ];
+let users = [
+  { username: 'sanjeev', password: 'sanjeev@12666' },
+  { username: 'yash', password: 'yash@12666' },
+  { username: 'prakhar', password: 'prakhar@12666' },
+  { username: 'test', password: 'test@12666' },
+  { username: 'admin', password: 'admin@12666' }
+];
+
+const addUser = (username, password) => {
+  users.push({ username, password });
+};
+
+module.exports = {
+  users,
+  addUser
+};
